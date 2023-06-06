9cf144a2fba0e4bb0c64833b38893896c39283e6
윤채민 <yooune.01@gmail.com>
2023-06-07 04:43:12 +0900

    Implement feature: Sort all recipes by descending order

--- capstone/src/main/java/impact/capstone/recipe/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package impact.capstone.recipe.repository;
-
-import impact.capstone.recipe.model.entity.RecipeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<RecipeEntity, String> {
-}
