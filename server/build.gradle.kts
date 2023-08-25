d48fb345d42b39317f4e71f5eaea8fda5ab3f91f
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-08-26 02:09:45 +0900

    프로젝트 생성

---
+++ server/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	// Spring Boot
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	// KAPT(Kotlin Annotation Processor)
+	id("org.jetbrains.kotlin.kapt") version "1.8.22"
+	// Kotlin
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.jpa") version "1.8.22"
+}
+
+group = "deu.capstone.impact"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	//spring-boot
+	implementation ("org.springframework.boot:spring-boot-starter-web")
+	testImplementation ("org.springframework.boot:spring-boot-starter-test")
+	developmentOnly ("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation ("com.fasterxml.jackson.module:jackson-module-kotlin")
+	//kotlin
+	implementation ("org.jetbrains.kotlin:kotlin-reflect")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
